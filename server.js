723d436d156678c496d1ec65d01c9c86ff2ffdd9
rootsofworld <faith74731@hotmail.com.tw>
2019-04-17 10:04:56 +0800

    webpack config

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 let app = express()
 
 const port = (process.argv[2]) ? process.argv[2] : 3000;
+
+app.use(express.static(path.resolve('static')))
 
 app.listen(port, (err) => {
     console.info("Server listening on port : %d", port)
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: 'main.js',
+        path: path.resolve('static/js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }]
+            }
+        ]
+    }
+}
